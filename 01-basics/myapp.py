c928ce635bce4fd04ffd772c8f3a0a843439af1b
Krysa77 <118102476+Krysa77@users.noreply.github.com>
2024-10-01 14:42:38 +0200

    asi jedna uplod

---
+++ 01-basics/myapp.py
@@ -0,0 +1,18 @@
+print("kviz")
+answer = int(input("kolik ma svab nohou?"))
+if answer == 8:
+    print("spravne")
+else:
+    print("spatne, svab ma 8 nohou")
+
+x = input("proc je obloha modra"); y = "sam nevim"
+print('vase odpoved "{}" a odpoved autora je "{}"'.format(x, y))
+print("pomoc, nevim co delam hihi\n")
+
+print("co rad posloucha matej fojtik:\n A) pop\n C) metal\n D) rap")
+mato = input()
+if mato == "C":
+    print("ano moc spravne")
+else:
+    print("tohle asi ne\n\n\n\n\n\n")
+print("ukkonce moje trapeni ")
